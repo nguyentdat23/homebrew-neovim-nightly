59336df5bd1abfbd8dc65c9d7024918574ddd10a
Nguyen Thanh Dat <47821599+nguyentdat23@users.noreply.github.com>
2025-05-05 10:53:45 +0700

    Update neovim-nightly.rb

--- Casks/neovim-nightly.rb
+++ Casks/neovim-nightly.rb
@@ -2,17 +2,25 @@ cask "neovim-nightly" do
   version :latest
   sha256 :no_check
 
-  arch arm: "arm64", intel: "x86_64"
-  url "https://github.com/neovim/neovim/releases/download/nightly/nvim-macos-#{arch}.tar.gz",
-      verified: "github.com/neovim"
+  on_intel do
+    arch = "x86_64"
+    url "https://github.com/neovim/neovim/releases/download/nightly/nvim-macos-#{arch}.tar.gz",
+        verified: "github.com/neovim"
+    binary "nvim-macos-#{arch}/bin/nvim"
+  end
+
+  on_arm do
+    arch = "arm64"
+    url "https://github.com/neovim/neovim/releases/download/nightly/nvim-macos-#{arch}.tar.gz",
+        verified: "github.com/neovim"
+    binary "nvim-macos-#{arch}/bin/nvim"
+  end
 
   name "Neovim"
   desc "Vim-fork focused on extensibility and usability"
   homepage "https://neovim.io"
 
   conflicts_with formula: "neovim"
-
-  binary "nvim-macos-#{arch}/bin/nvim"
 
   postflight do
     system_command "xattr", args: ["-cr", "#{staged_path}"]
